505760561ec71112d5ccdbc4c506273a1779dd4e
ind0zer <165696434+ind0zer@users.noreply.github.com>
2024-05-23 22:40:14 +0300

    finish

---
+++ main.js
@@ -0,0 +1,58 @@
+// 1 excersise
+const fahrenheit = 77;
+const celsius = 25;
+console.log("1 excersise");
+console.log(((fahrenheit - 32) * 5) / 9);
+
+// 2 excersise
+console.log("2 excersise");
+const month = 31;
+// console.log(month * 24)
+const monthHours = 744;
+// console.log(24*60);
+const monthMinute = 1440;
+
+console.log("In 31 days :", monthHours, "hours");
+console.log("In 31 days :", monthMinute, "minutes");
+
+// 3 excersise
+
+const health = 100;
+const energy = 100;
+
+console.log("3 excersise");
+console.log("Your health is:", health - 34);
+console.log("Your energy is:", energy - 71);
+
+// 4 excersise
+console.log("4 excersise");
+const buyPrice = 9365;
+console.log(buyPrice * 0.1);
+
+// 5 excersise
+console.log("5 excersise");
+const numberComa = 199.6;
+console.log(Math.floor(numberComa))
+
+// 6 excersise
+console.log("6 excersise");
+const biggerNumberString = "9.6";
+console.log(parseFloat(biggerNumberString));
+
+// 7 excersise
+console.log("7 excersise");
+const parseIntNumber = "9";
+console.log(parseInt(parseIntNumber));
+
+// 8 excersise
+console.log("8 excersise");
+const sqrt = 169;
+console.log(Math.sqrt(sqrt))
+
+// 9 excersise
+console.log("9 excersise");
+const integer = 100;
+const stringNumber = "100";
+
+console.log(parseInt(stringNumber));
+console.log(integer.toString());
